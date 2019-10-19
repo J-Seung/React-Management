51a8510c6cf7f8c22ae5f63e06d573b5720a0f70
Sseung <sseung@Sseungui-MacBookPro.local>
2019-10-19 15:58:56 +0900

    Update Customer.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,45 @@ import React from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  name : '홍길동',
-  birthday : '961222', 
-  gender : '남자',
-  job : '대학생'
-}
+const customers = [
+  {
+    id : 1,
+    img : "https://placeimg.com/64/64/1",
+    name : '홍길동',
+    birthday : '961222', 
+    gender : '남자',
+    job : '대학생'
+  }, 
+  {
+    id : 2,
+    img : "https://placeimg.com/64/64/2",
+    name : '이재승',
+    birthday : '950525', 
+    gender : '남자',
+    job : '프로그래머'
+  },
+  {
+    id : 3,
+    img : "https://placeimg.com/64/64/3",
+    name : '이순신',
+    birthday : '990822', 
+    gender : '남자',
+    job : '대학생'
+  }
+]
 
 function App() {
   return (
-    <Customer name={customer.name} birthday={customer.birthday} gender={customer.gender} job={customer.job}/>
+    <div>
+      {
+        customers.map(c=> {
+          return (
+            <Customer key={c.id} id={c.id} img={c.img} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+          )
+        })
+      
+      }
+    </div>  
   );
 }
 

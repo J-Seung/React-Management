a15fcdc969f7e56dd6a699630307ad4fd3888831
Sseung <sseung@Sseungui-MacBookPro.local>
2019-10-19 16:38:43 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,47 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
-import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+root: {
+width: "100%",
+marginTop: theme.spacing.unit * 3,
+overflowX: "auto"
+},
+table: {
+minWidth: 1080
+}
+});
 
 const customers = [
-  {
-    id : 1,
-    img : "https://placeimg.com/64/64/1",
-    name : '홍길동',
-    birthday : '961222', 
-    gender : '남자',
-    job : '대학생'
-  }, 
-  {
-    id : 2,
-    img : "https://placeimg.com/64/64/2",
-    name : '이재승',
-    birthday : '950525', 
-    gender : '남자',
-    job : '프로그래머'
-  },
-  {
-    id : 3,
-    img : "https://placeimg.com/64/64/3",
-    name : '이순신',
-    birthday : '990822', 
-    gender : '남자',
-    job : '대학생'
-  }
+{
+'id': 1,
+'img': 'https://placeimg.com/48/48/1',
+'name': '홍길동',
+'birthday': '961222',
+'gender': '남자',
+'job': '대학생'
+},
+{
+'id': 2,
+'img': 'https://placeimg.com/48/48/2',
+'name': '나동빈',
+'birthday': '960508',
+'gender': '남자',
+'job': '프로그래머'
+},
+{
+'id': 3,
+'img': 'https://placeimg.com/48/48/3',
+'name': '이순신',
+'birthday': '961127',
+'gender': '남자',
+'job': '디자이너'
+}
 ]
 
-function App() {
-  return (
-    <div>
-      {
-        customers.map(c=> {
-          return (
-            <Customer key={c.id} id={c.id} img={c.img} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
-          )
-        })
-      
-      }
-    </div>  
-  );
+class App extends Component {
+render() {
+const { classes } = this.props;
+return (
+<Paper className={classes.root}>
+<Table className={classes.table}>
+<TableHead>
+<TableRow>
+<TableCell>번호</TableCell>
+<TableCell>이미지</TableCell>
+<TableCell>이름</TableCell>
+<TableCell>생년월일</TableCell>
+<TableCell>성별</TableCell>
+<TableCell>직업</TableCell>
+</TableRow>
+</TableHead>
+<TableBody>
+{customers.map(c => {
+return <Customer key={c.id} id={c.id} img={c.img} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+})}
+</TableBody>
+</Table>
+</Paper>
+);
+}
 }
 
-export default App;
+export default withStyles(styles)(App);

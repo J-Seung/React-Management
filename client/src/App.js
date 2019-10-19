f4bd6d62cade78e4333ed18fe45b5a22dbe4ccd4
Sseung <sseung@Sseungui-MacBookPro.local>
2019-10-19 17:51:47 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,58 +20,49 @@ minWidth: 1080
 }
 });
 
-const customers = [
-{
-'id': 1,
-'img': 'https://placeimg.com/48/48/1',
-'name': '홍길동',
-'birthday': '961222',
-'gender': '남자',
-'job': '대학생'
-},
-{
-'id': 2,
-'img': 'https://placeimg.com/48/48/2',
-'name': '나동빈',
-'birthday': '960508',
-'gender': '남자',
-'job': '프로그래머'
-},
-{
-'id': 3,
-'img': 'https://placeimg.com/48/48/3',
-'name': '이순신',
-'birthday': '961127',
-'gender': '남자',
-'job': '디자이너'
-}
-]
 
 class App extends Component {
-render() {
-const { classes } = this.props;
-return (
-<Paper className={classes.root}>
-<Table className={classes.table}>
-<TableHead>
-<TableRow>
-<TableCell>번호</TableCell>
-<TableCell>이미지</TableCell>
-<TableCell>이름</TableCell>
-<TableCell>생년월일</TableCell>
-<TableCell>성별</TableCell>
-<TableCell>직업</TableCell>
-</TableRow>
-</TableHead>
-<TableBody>
-{customers.map(c => {
-return <Customer key={c.id} id={c.id} img={c.img} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-})}
-</TableBody>
-</Table>
-</Paper>
-);
-}
+
+    state = {
+        customers : ""
+    }
+
+    componentDidMount() {
+        this.callApi()
+        .then(res => this.setState({customers: res}))
+        .catch(err => console.log(err));
+    }
+
+    callApi = async () => {
+        const response = await fetch('/api/customers');
+        const body = await response.json();
+        return body;
+    }
+
+    render() {
+    const { classes } = this.props;
+        return (
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>번호</TableCell>
+                            <TableCell>이미지</TableCell>
+                            <TableCell>이름</TableCell>
+                            <TableCell>생년월일</TableCell>
+                            <TableCell>성별</TableCell>
+                            <TableCell>직업</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.customers ? this.state.customers.map(c => {
+                        return <Customer key={c.id} id={c.id} img={c.img} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+                        }) : ""}
+                    </TableBody>
+                </Table>
+            </Paper>
+        );
+    }
 }
 
 export default withStyles(styles)(App);
